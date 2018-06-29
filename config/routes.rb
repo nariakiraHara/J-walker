6ac4a2568bcf2c2a81f3573cded991814e8f2df6
Daiki Takaya <takaya@sonicgarden.jp>
2018-06-29 22:46:39 +0900

    いらないcontrolelr削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   get 'prifiles/profile'
-  devise_for :users, :controllers => {
-    :registrations => 'users/registrations'
-  }
+  devise_for :users
   root 'homes#index'
   get 'homes/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
