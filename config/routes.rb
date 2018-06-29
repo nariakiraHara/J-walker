db43e49861044d7a1701b6a77f455034053762be
Daiki Takaya <takaya@sonicgarden.jp>
2018-06-29 22:55:44 +0900

    usersページのurlを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  resources :users
   get 'prifiles/profile'
   devise_for :users
   root 'homes#index'
-  get 'homes/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
